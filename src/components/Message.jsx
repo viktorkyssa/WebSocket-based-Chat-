94375ceb142f599931ef3cda1c2e7ec09617b340
Viktor Kyssa <vkibank@gmail.com>
2019-11-26 21:27:41 +0200

    containers

---
+++ src/components/Message.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropsTypes from 'prop-types';
+
+const Message = ({author, message}) => (
+    <p>
+        <i>{author}</i>
+        {message}
+    </p>
+);
+
+Message.propsTypes = {
+    author: PropsTypes.string.isRequired,
+    message: PropsTypes.string.isRequired
+};
+
+export default Message;
--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
-const MessagesList = props => {
+import Message from "./Message";
+
+const MessagesList = ({ messages }) => {
 	return(
-		<section id="messages-list">Messages List</section>
+		<section id="messages-list">
+			<ul>
+				{messages.map(message => <Message key={message.id} {...message} />)}
+			</ul>
+		</section>
 	)
+};
+
+MessagesList.propTypes = {
+	messages: PropsTypes.arrayOf(
+		PropsTypes.shape({
+			id: PropsTypes.number.isRequired,
+			message: PropsTypes.string.isRequired,
+			author: PropsTypes.string.isRequired
+		}).isRequired
+	).isRequired
 };
 
 export default MessagesList;
